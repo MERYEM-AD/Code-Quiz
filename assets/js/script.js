67cda52ad0b5abae37ea68a01667b9c15baa87aa
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-10-04 18:17:51 -0400

    set up timer for the quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const startButton = document.querySelector("#startButton");
 const Presentation= document.querySelector(".Presentation");
 let Test = document.querySelector(".Quiz");
+let Timer = document.querySelector(".Timer");
 startButton.addEventListener("click",function(){
 
 
@@ -12,8 +13,37 @@ startButton.addEventListener("click",function(){
 //create element
 function Quiz(){
    // Test.setAttribute();
-    let headLine = document.createElement("h2");
+    setTime();
+    const headLine = document.createElement("h2");
     headLine.textContent="The Quiz is beging ! Good Luck";
     Test.appendChild(headLine);
+
 }
 
+//Set Up the Timer
+
+let timeLeft = 10;
+
+function setTime() {
+    // Sets interval in variable
+    let timerInterval = setInterval(function() {
+        timeLeft--;
+        Timer.textContent = timeLeft +" s"
+      if(timeLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to create and append image
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+
+
+  function sendMessage(){
+    Timer.textContent = "-- : --";
+
+    Test.textContent= "    G A M E  O V E R   ";
+
+
+  }
